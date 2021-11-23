f8b1e96af27c1118a927b43d49e85ec1556517ec
oleksandrilchuck <64355157+oleksandrilchuck@users.noreply.github.com>
2021-11-24 00:49:57 +0200

    Адаптивне випадаюче меню

---
+++ adaptive menu deeph/js/main.js
@@ -0,0 +1,7 @@
+// додає класи боді під час спрацьовування бургеру
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
